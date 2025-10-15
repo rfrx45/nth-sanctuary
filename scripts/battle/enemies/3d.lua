103b907af1f3cb336fc8a0bab8d139c54bcef2b7
DIAMONDDELTAHEDRON <183671108+DIAMONDDELTAHEDRON@users.noreply.github.com>
2025-10-15 00:48:36 -0700

    Update 3d.lua

--- scripts/battle/enemies/3d.lua
+++ scripts/battle/enemies/3d.lua
@@ -77,7 +77,21 @@ function ThreeDPrism:onAct(battler, name)
         battler:setAnimation("pirouette")
 		return "* Kris spun around three-dimensionally!"
     elseif name == "Standard" then --X-Action
-        super.onShortAct(self, battler, name)
+        Assets.stopAndPlaySound("pirouette", 0.7, 1.1)
+        battler:setAnimation("pirouette")
+        if battler.chara.id == "ralsei" then
+			self:addMercy(3)
+			return "* Ralsei demonstrates 3D rotation!"
+        elseif battler.chara.id == "susie" then
+			self:addMercy(2)
+			return "* Susie spun like a turntable!"
+		elseif battler.chara.id == "jamm" then
+			self:addMercy(2)
+			return "* Jamm revolved around the Z-axis!"
+		else
+			self:addMercy(2)
+			return "* "..battler.chara:getName().." spun around in 3D!"
+		end
     end
 
     -- If the act is none of the above, run the base onAct function
