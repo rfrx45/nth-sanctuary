0b0e8b4293b75cb16e44ad1e256d806517c844e3
DIAMONDDELTAHEDRON <183671108+DIAMONDDELTAHEDRON@users.noreply.github.com>
2025-10-16 16:22:59 -0700

    Update kris.lua

--- scripts/data/actors/kris.lua
+++ scripts/data/actors/kris.lua
@@ -8,7 +8,31 @@ function actor:init()
     })
     TableUtils.merge(self.offsets, {
         ["pirouette"] = {5, 0},
+        -- TODO: Accuracy.
+        ["climb/climb"] = {0, 0},
+        ["climb/charge"] = {0, 3},
+        ["climb/slip_left"] = {0, 0},
+        ["climb/slip_right"] = {0, 0},
+        ["climb/slip_fall"] = {0, 0},
+        ["climb/land_left"] = {0, 0},
+        ["climb/land_right"] = {0, 0},
+        ["climb/jump_up"] = {0, 0},
+        ["climb/jump_left"] = {0, 0},
+        ["climb/jump_right"] = {0, 0},
     })
+end
+
+-- TODO: This sucks.
+function actor:getOffset(sprite)
+    local ox, oy = super.getOffset(self, sprite)
+    if StringUtils.startsWith(sprite, "climb") then
+        ox, oy = ox - 5, oy + 4
+    end
+    return ox, oy
+end
+
+function actor:hasOffset(sprite)
+    if super.hasOffset(self, sprite) then return true end
 end
 
 return actor
