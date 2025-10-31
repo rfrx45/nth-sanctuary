18fd41500877cd2418ede0848c08323df6d29e7a
DIAMONDDELTAHEDRON <183671108+DIAMONDDELTAHEDRON@users.noreply.github.com>
2025-10-30 19:34:41 -0700

    Remove Guei from base mod
    (since CH4EnemyRecreations adds it anyway)

--- mod.lua
+++ mod.lua
@@ -59,19 +59,6 @@ function Mod:postInit(new_file)
         Game.world:startCutscene("primary.intro")
 		Game:setFlag("ft_last_map", "base_sanctum_center")
     end
-    HookSystem.hook(Battle, "spawnEnemyTextbox", function(orig, self, enemy, ...)
-        if enemy and enemy.id == "guei" then
-            local x, y = enemy.sprite:getRelativePos(0, enemy.sprite.height/2, self)
-            if enemy.dialogue_offset then
-                x, y = x + enemy.dialogue_offset[1], y + enemy.dialogue_offset[2]
-            end
-            local textbox = GueiTextbox(x, y, enemy.balloon_type)
-            self:addChild(textbox)
-            return textbox
-        else
-            return orig(self, enemy, ...)
-        end
-    end)
 	Mod.titan_dissolve_shader = love.graphics.newShader[[
     extern float amount;
     vec4 effect(vec4 color, Image tex, vec2 texture_coords, vec2 screen_coords) {
